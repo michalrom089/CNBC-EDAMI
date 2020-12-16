b83cc3be8aa619d65d0d8275727e990b14b8d2ec
Michal Romanowski <michal.rom089@gmial.com>
2020-12-16 19:41:47 +0100

    add knn implementation

--- cnbc/cnbc.py
+++ cnbc/cnbc.py
@@ -23,7 +23,7 @@ def eps_neighborhood(data, p_idx, eps=0.3, dist_method="euclidean"):
             points indx that meet the condition of min eps
     """
 
-    dist = cdist(data, [data.iloc[p_idx]])
+    dist = cdist(data, [data.iloc[p_idx]], metric=dist_method)
     # reduce dimensions (cdist produces ''matrix'')
     dist = dist[:, 0]
 
@@ -33,14 +33,45 @@ def eps_neighborhood(data, p_idx, eps=0.3, dist_method="euclidean"):
     return q_without_p
 
 
-def rknn(data, p):
-    """ reversed  punctured k+-neighborhood  of  a  point p
+def k_neighborhood(data, p_idx, k, dist_method="euclidean"):
+    """ k-neighborhood or kNN(p)
+
+    Args:
+        data : pd.DataFrame
+        p_idx : int
+            index of the considered point
+        k : int
+            number of points satisfying the kNN(p) condition
+
+    Return:
+        np.array :
+            points indx that meet the condition kNN(p) condition
+
+    @see: http://ceur-ws.org/Vol-1269/paper113.pdf Section 3, Definition 2 
     """
-    pass
+
+    assert k > 0
+
+    dist = cdist(data, [data.iloc[p_idx]], metric=dist_method)
+    # reduce dimensions (cdist produces ''matrix'')
+    dist = dist[:, 0]
+    # sort indexes of dist in a ascending order
+    dist_idx_asc = np.argsort(dist)
+    # remove p_idx from sorted indexes
+    dist_idx_asc = np.delete(dist_idx_asc, p_idx)
+
+    # return first k indexes
+    return dist_idx_asc[:k]
 
 
 def kknn(data, p):
     """ punctured k+-neighborhood
+    """
+    pass
+
+
+def rknn(data, p):
+    """ reversed  punctured k+-neighborhood  of  a  point p
     """
     pass
 
@@ -54,6 +85,6 @@ if __name__ == "__main__":
     cl = iris["class"]
 
     data = iris[["sepallength", "sepalwidth", "petallength", "petalwidth"]]
-    eps_n = eps_neighborhood(data, 0)
+    eps_n = k_neighborhood(data.head(), 0, 2)
 
     print(eps_n)
